0a169901b356d8ac8def085e2c453002294ef6a3
DanielMcCoy <ammkisdmbabe143@gmail.com>
2019-11-02 11:10:29 +0630

    After completing app frame

---
+++ app/src/main/java/com/cookbook/mealsdetails/MealsDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.cookbook.mealsdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MealsDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meals_details)
+    }
+}
